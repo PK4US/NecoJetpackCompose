252b48e97db5c3d5fbf1942cb84aa550b6fdd7e8
PK4US <Stepanchuk.ei@gmail.com>
2023-06-12 11:40:12 +0300

    L5_State

--- app/src/main/java/com/pk4us/necojetpackcompose/MainActivity.kt
+++ app/src/main/java/com/pk4us/necojetpackcompose/MainActivity.kt
@@ -3,15 +3,20 @@ package com.pk4us.necojetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.pk4us.necojetpackcompose.components.MyL1Text
 import com.pk4us.necojetpackcompose.components.MyL2ColumnRow
 import com.pk4us.necojetpackcompose.components.MyL3CardBoxImage
 import com.pk4us.necojetpackcompose.components.MyL4Modifier
+import com.pk4us.necojetpackcompose.components.MyL5State
 import com.pk4us.necojetpackcompose.components.listItem
 import com.pk4us.necojetpackcompose.ui.theme.MyDefaultMaterialTheme
 
@@ -21,13 +26,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyDefaultMaterialTheme() {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-//                    MyL1Text()
-//                    MyL2ColumnRow()
-//                    MyL3CardBoxImage()
-                    MyL4Modifier()
+                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+//                        MyL1Text()
+//                        MyL2ColumnRow()
+//                        MyL3CardBoxImage()
+//                        MyL4Modifier()
+                        MyL5State()
+                    }
                 }
             }
         }
---
+++ app/src/main/java/com/pk4us/necojetpackcompose/components/L5_State.kt
@@ -0,0 +1,44 @@
+package com.pk4us.necojetpackcompose.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+@Preview
+fun MyL5State() {
+    val counter = remember { mutableIntStateOf(0) }
+    val color = remember { mutableStateOf(Color.Blue) }
+
+    Box(
+        modifier = Modifier
+            .size(100.dp)
+            .background(color = color.value, shape = CircleShape)
+            .clickable {
+                when (++counter.value) {
+                    10 -> color.value = Color.Green
+                }
+            }, contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = counter.value.toString(),
+            style = TextStyle(color = Color.White),
+            fontSize = 20.sp
+        )
+    }
+}
